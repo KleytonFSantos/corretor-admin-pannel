fa8e0463ee3e0708fb646c4dd5b0544610928e73
kleytonsantos <akatsukipb12@gmail.com>
2024-06-09 05:30:44 -0300

    feat: criando formulário de adicionar corretor

--- app/Filament/Pages/Dashboard/Corretores.php
+++ app/Filament/Pages/Dashboard/Corretores.php
@@ -3,49 +3,66 @@
 namespace App\Filament\Pages\Dashboard;
 
 use App\Models\User;
-use Filament\Forms\Components\Section;
-use Filament\Tables\Actions\Action;
-use Filament\Facades\Filament;
-use Filament\Forms\Components\Repeater;
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\Action;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
-use Filament\Forms\Form;
 use Filament\Pages\Page;
-use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\Hash;
 
-class Corretores extends Page implements HasForms, HasTable
+class Corretores extends Page implements HasForms, HasTable, HasActions
 {
     use InteractsWithForms;
     use InteractsWithTable;
+    use InteractsWithActions;
 
     public ?array $data = [];
+
+    public ?array $formData = [];
+
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.dashboard.corretores';
-
-    public User $user;
-
-    public function mount(): void
-    {
-        $this->user = Filament::getCurrentPanel()->auth()->user();
-
-        $this->form->fill(['teste' => 'testando']);
-
-    }
 
     public function table(Table $table): Table
     {
         return $table
             ->query(User::query())
             ->headerActions([
-                Action::make('Adicionar')
+                CreateAction::make('Adicionar')
+                    ->label('Novo Corretor')
                     ->color('success')
+                    ->form([
+                        TextInput::make('name')
+                            ->label('Nome')
+                            ->required(),
+                        TextInput::make('email')
+                            ->label('Email')
+                            ->email()
+                            ->required(),
+                        TextInput::make('cpf')
+                            ->label('CPF')
+                            ->required(),
+                        TextInput::make('password')
+                            ->label('password')
+                            ->dehydrateStateUsing(fn (string $state): string => Hash::make($state))
+                            ->dehydrated(fn (?string $state): bool => filled($state))
+                            ->required(),
+                        Select::make('roles')
+                            ->options([
+                                'corretor' => 'Corretor',
+                                'super_admin' => 'Administrador',
+                            ])
+                            ->required(),
+                    ]),
             ])
             ->columns([
                 TextColumn::make('email'),
